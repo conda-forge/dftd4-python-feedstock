2c548bbd885e551a041a07b78169a40720883049
Sebastian Ehlert <sehlert@microsoft.com>
2024-09-15 16:36:03 +0200

    Add migration for Python 3.13

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,30 +2,25 @@
 
 set -ex
 
-pushd python
-
-if [[ "${CONDA_BUILD_CROSS_COMPILATION:-0}" != "1" ]]; then
-  MESON_ARGS=${MESON_ARGS:---prefix=${PREFIX} --libdir=lib}
-else
-  cat > pkgconfig.ini <<EOF
+cat > ${SRC_DIR}/pkgconfig.ini <<EOF
 [binaries]
 pkgconfig = '$BUILD_PREFIX/bin/pkg-config'
 EOF
-  MESON_ARGS="${MESON_ARGS:---prefix=${PREFIX} --libdir=lib} --cross-file pkgconfig.ini"
+
+build_args=(
+  "-Csetup-args=--warnlevel=0"
+  "-Csetup-args=--cross-file=${SRC_DIR}/pkgconfig.ini"
+  "-Csetup-args=-Dpython_version=${PREFIX}/bin/python3"
+)
+if [ -f ${BUILD_PREFIX}/meson_cross_file.txt ]; then
+  build_args=("${build_args[@]}" "-Csetup-args=--cross-file=${BUILD_PREFIX}/meson_cross_file.txt")
 fi
 
-# HACK: extend $CONDA_PREFIX/meson_cross_file that's created in
-# https://github.com/conda-forge/ctng-compiler-activation-feedstock/blob/main/recipe/activate-gcc.sh
-# https://github.com/conda-forge/clang-compiler-activation-feedstock/blob/main/recipe/activate-clang.sh
-# to use host python; requires that [binaries] section is last in meson_cross_file
-echo "python = '${PREFIX}/bin/python'" >> ${CONDA_PREFIX}/meson_cross_file.txt
+${PYTHON} -m build \
+    "${build_args[@]}" \
+    --outdir . \
+    --no-isolation \
+    --wheel \
+    python/
 
-meson setup _build \
-  ${MESON_ARGS} \
-  --buildtype=release \
-  --warnlevel=0
-
-meson compile -C _build
-meson install -C _build --no-rebuild
-
-popd
+${PYTHON} -m pip install *.whl --no-deps -vv
